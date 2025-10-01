0417f52fe9abcf23cd8fa5d21ea2125c24aed32c
Phat117 <ntphat110705@gmail.com>
2025-10-01 22:42:19 +0700

    Create Stripe Implementing createCheckoutSession

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@/app/generated/prisma";
 import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
@@ -8,3 +9,55 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
         apiVersion: "2025-09-30.clover",
         typescript: true,
 });
+export type OrderWithItemsAndProduct = Prisma.OrderGetPayload<{
+        include: {
+                items: {
+                        include: {
+                                product: true;
+                        };
+                };
+        };
+}>;
+
+export async function createCheckoutSession(order: OrderWithItemsAndProduct) {
+        if (!order.items || order.items.length === 0) throw new Error("No items in order");
+
+        const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = order.items.map((item) => {
+                return {
+                        price_data: {
+                                currency: "usd",
+                                product_data: {
+                                        name: item.product.name,
+                                        description: item.product.description || "",
+                                        images: [item.product.image || ""],
+                                },
+                                unit_amount: item.product.price * 100,
+                        },
+                        quantity: item.quantity,
+                };
+        });
+
+        const successUrl = `${process.env.NEXT_PUBLIC_URL}/order/success?session_id={CHECKOUT_SESSION_ID}`;
+        const cancelUrl = `${process.env.NEXT_PUBLIC_URL}/cart?cancel=true`;
+
+        try {
+                // Create checkout session
+                const session = await stripe.checkout.sessions.create({
+                        payment_method_types: ["card"],
+                        mode: "payment",
+                        line_items,
+                        success_url: successUrl,
+                        cancel_url: cancelUrl,
+                        metadata: {
+                                orderId: order.id.toString(),
+                        },
+                });
+                return {
+                        sessionId: session.id,
+                        sessionUrl: session.url,
+                };
+        } catch (error) {
+                console.error("Error creating checkout session", error);
+                throw new Error("Failed to create checkout session");
+        }
+}
