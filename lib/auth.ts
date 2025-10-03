92f6373f08ed10d54012cd3600088d92471bd027
Phat117 <ntphat110705@gmail.com>
2025-10-03 19:51:50 +0700

    Credentials Provider Logic

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,47 @@
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import db from "./db";
+import { signInSchema } from "./schemas";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-        providers: [],
+        providers: [
+                Credentials({
+                        name: "Credentials",
+                        credentials: {
+                                email: { label: "Email", type: "email" },
+                                password: { label: "Password", type: "password" },
+                        },
+                        async authorize(credentials) {
+                                const parseCredentials = signInSchema.safeParse(credentials);
+
+                                if (!parseCredentials.success) {
+                                        console.log("Invalid credentials");
+                                        return null;
+                                }
+
+                                const { email, password } = parseCredentials.data;
+
+                                const user = await db.user.findUnique({
+                                        where: { email },
+                                });
+
+                                if (!user) {
+                                        console.log("User not found");
+                                        return null;
+                                }
+
+                                const passwordsMatch = await verifyPassword(password, user.password);
+
+                                if (!passwordsMatch) {
+                                        console.log("Invalid password");
+                                        return null;
+                                }
+
+                                return user;
+                        },
+                }),
+        ],
         pages: {
                 signIn: "/auth/signin",
         },
